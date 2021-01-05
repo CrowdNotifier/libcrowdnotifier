df44f798d1a0f3e19886dfc7d8fb6561aab30e3f
Linus Gasser <linus.gasser@epfl.ch>
2021-01-05 13:29:50 +0100

    Adding app/index.ts, v1.0.2

---
+++ app/index.ts
@@ -0,0 +1,12 @@
+import * as v1 from './v1';
+import * as v1_1 from './v1_1';
+import {HealthAuthority, Location, Visit} from './v2';
+
+import {mcl, sodium} from '@c4dt/libcrowdnotifier';
+
+export {
+  HealthAuthority, Location, Visit,
+  v1, v1_1,
+
+  mcl, sodium,
+};
