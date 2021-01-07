1129bf53e085393af1d22463602e5c9fbf13bc36
Linus Gasser <linus.gasser@epfl.ch>
2021-01-07 16:33:59 +0100

    Wouters' comments

--- lib/src/v2/index.ts
+++ lib/src/v2/index.ts
@@ -4,11 +4,12 @@ import {waitReady, IEncryptedData} from './ibe_primitives';
 import {ILocationData} from './structs';
 import {PreTraceWithProof, PreTrace, TraceProof, Trace, QRCodeTrace,
   QRCodeEntry, QRCodeContent, MasterTrace} from './proto';
+import {baseG1, baseG2, genId} from './helpers';
 
 export {
   // CrowdNotifierPrimitives
   setupHA, genCode, scan, genPreTrace, genTrace, verifyTrace, match,
-  waitReady, IEncryptedData,
+  waitReady, IEncryptedData, baseG1, baseG2, genId,
   // Proto structures needed
   PreTraceWithProof, Trace, PreTrace, TraceProof,
   QRCodeTrace, QRCodeEntry, QRCodeContent, MasterTrace,
